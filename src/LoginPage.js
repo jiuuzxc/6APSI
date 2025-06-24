650fba80d70532ea0b29673eb0dd58cf67843aba
Julian <jmguintu3@student.hau.edu.ph>
2025-06-24 21:11:56 +0800

    add login username input, password input, and login button

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,21 +1,48 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import './LoginPage.css';
+import './LandingPage.js';
 
 function LoginPage() {
- 
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    if (username == 'guest' && password == 'abcd') {
+      navigate('/landing-page');
+    } else {
+      alert("Invalid Login Credentials");
+    }
+  };
+
   return (
-    <div>
-      <h2>Login</h2>
-      <input
-        type="text"
-        placeholder="Username"
-       
-      /><br />
-      <input
-        type="password"
-        placeholder="Password"
-     
-      /><br />
-      <button >Login</button>
+    <div className='background'>
+
+      <div className="loginContainer">
+        
+        <h2>Login</h2>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        /><br />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        /><br />
+
+
+        <button className="loginButton" onClick={handleLogin}>Login</button>
+
+
+
+      </div>
     </div>
   );
 }
