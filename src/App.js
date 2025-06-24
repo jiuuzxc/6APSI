8d8e94a3f6c62a30a5807d376f217a753b24ff58
Julian <jmguintu3@student.hau.edu.ph>
2025-06-24 18:15:07 +0800

    add routing and navigation in app.js for login and landing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import LoginPage from './LoginPage'; 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import LoginPage from './LoginPage.js';
+import LandingPage from './LandingPage.js';
 
 function App() {
   return (
     <div>
-      <LoginPage />
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/landing-page" element={<LandingPage />} />
+      </Routes>
+    </Router>
     </div>
   );
 }
